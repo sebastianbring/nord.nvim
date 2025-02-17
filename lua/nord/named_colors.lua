796e6cdcfe036ec3a950957674444abfe29219b7
Sebastian Ring <sebastian.ring@bring.com>
2025-02-17 14:15:19 +0100

    tst

--- lua/nord/named_colors.lua
+++ lua/nord/named_colors.lua
@@ -1,6 +1,6 @@
 local nord = {
 	--16 colors
-	black = "#2E3440", -- nord0 in palette
+	black = "#000000", -- nord0 in palette
 	dark_gray = "#3B4252", -- nord1 in palette
 	gray = "#434C5E", -- nord2 in palette
 	light_gray = "#4C566A", -- nord3 in palette
